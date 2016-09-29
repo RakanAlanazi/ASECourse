6efca3aa3f8a50eff4f18e9f20d9e34aef8311ff
RakanAlanazi <a.rakan@outlook.com>
2016-09-28 20:41:06 -0500

    HW 5

---
+++ Assignment-5/Source/RakanApp/www/js/controllers.js
@@ -0,0 +1,106 @@
+angular.module('starter.controllers', [])
+
+
+  .controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
+    $scope.data = {};
+
+    $scope.registration = function() {
+
+      $state.go('registration');
+    };
+    $scope.login = function() {
+      LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+        $state.go('tab.home');
+      }).error(function(data) {
+        var alertPopup = $ionicPopup.alert({
+          title: 'Login failed!',
+          template: 'Please check your credentials!'
+        });
+      });
+    }
+  })
+
+  .controller('DashCtrl', function($scope, $http) {
+  $scope.getWeather=function() {
+
+  var resp = $http.get("http://api.openweathermap.org/data/2.5/weather?q="+$scope.city+"&units=imperial&appid=a4e90c978599d4b8759f03bd61515911");
+  resp.success(function(data,status,headers,config){
+    console.log("data : "+data.weather[0].id);
+    $scope.temperatureSource = data.main.temp;
+    $scope.pressureSource = data.main.pressure;
+    $scope.descSource = data.weather[0].description;
+
+  });
+  resp.error(function(data,status,headers,config){
+    alert("Failure during service call!!")
+  });
+};
+
+})
+
+.controller('ChatsCtrl',  function($scope,$http) {
+  $scope.venueList = new Array();
+  $scope.mostRecentReview;
+  $scope.getVenues = function () {
+    var placeEntered = document.getElementById("txt_placeName").value.replace(/\s/g, "");
+    if (placeEntered != null && placeEntered != "") {
+      document.getElementById('div_ReviewList').style.display = 'none';
+      //This is the API that gives the list of venues based on the place and search query.
+      var handler = $http.get("http://api.sqoot.com/v2/coupons?api_key=b61qrv&location="+placeEntered+"&per_page=5");
+      handler.success(function (data) {
+
+        if (data != null && data.coupons != null) {
+          for (var i = 0; i < data.coupons.length; i++) {
+            $scope.venueList[i] = {
+              "name": data.coupons[i].coupon.title,
+              "category": data.coupons[i].coupon.category_name,
+              "id": data.coupons[i].coupon.id,
+              "location": placeEntered
+            };
+          }
+        }
+        $scope.testLogin = function(k) {
+          var k = "Rakan"
+          return k;
+        }
+      })
+      handler.error(function (data) {
+        alert("There was some error processing your request. Please try after some time.");
+      });
+    }
+  }
+
+})
+
+  .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+
+  .controller('HomeCtrl', function($scope, $stateParams) {
+  })
+
+  .controller('RegisterCtrl', function($scope,$state) {
+    $scope.data = {};
+    $scope.registration = function() {
+
+      $state.go('registration');
+    }
+  })
+    .controller('AccountCtrl', function($scope,  $http) {
+        this.movieSearch = '';
+        this.formAction = function() {
+          $http.get('https://www.omdbapi.com/?s=' + this.movieSearch + '&y=&r=json').
+          then(function(response){
+            $scope.movies = response.data.Search
+          });
+        };
+        this.movieSelect = function(imdbID){
+          $http.get('https://www.omdbapi.com/?i=' + imdbID + '&y=&plot=short&r=json').
+          then(function(data){
+            console.log(data.data);
+            $scope.movie = data.data;
+          });
+        };
+
+      });
---
+++ Assignment-5/Source/RakanApp/www/js/jquery.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+  console.log("JS Loaded");
+  // Function to change size of search bar
+  var onSearch = function(){
+    $('.search').addClass("searchUp")
+    $('.movie-title').addClass("searchSize")
+  }
+
+  // run function onSearch when submit is clicked
+  $('#submit').click(function(){
+    onSearch();
+  })
+
+  // run function onSearch when enter is hit
+  $(".movie-title").keyup(function(ev) {
+    if (ev.which === 13 ) {
+      onSearch()
+    }
+  })
+
+  // run function onSearch when movie is clicked
+  $('#movieList').click(function(){
+    onSearch();
+    $('#movieList').addClass("moveLeft");
+  })
+
+});
